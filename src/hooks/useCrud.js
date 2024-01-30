a50901fc6f3ed789f94fbd3f4acc27d2e042ff8e
jduval15 <144378056+jduval15@users.noreply.github.com>
2024-01-30 15:47:43 -0400

    Update useCrud.js

--- src/hooks/useCrud.js
+++ src/hooks/useCrud.js
@@ -1,52 +1,48 @@
-import { useState } from "react"
-import axios from "axios"
-
+import { useState } from "react";
+import axios from "axios";
 
 const useCrud = (baseUrl) => {
+  const [infoApi, setInfoApi] = useState([]);
 
-    const [ infoApi, setInfoApi ] = useState()
+  // GET
+  const getApi = (path, errorCallback) => {
+    const url = `${baseUrl}${path}/`;
+    axios.get(url)
+      .then((res) => setInfoApi(res.data))
+      .catch((err) => errorCallback ? errorCallback(err) : console.error(err));
+  };
 
-    //GET
-    const getApi = (path) => {
-        const url = `${baseUrl}${path}/`
-        axios.get(url)
-        .then(res => setInfoApi(res.data))
-        .catch(err => console.log(err))
-    }
+  // POST
+  const postApi = (path, data, errorCallback) => {
+    const url = `${baseUrl}${path}/`;
+    axios.post(url, data)
+      .then((res) => {
+        setInfoApi((prevInfoApi) => [...prevInfoApi, res.data]);
+      })
+      .catch((err) => errorCallback ? errorCallback(err) : console.error(err));
+  };
 
-    // POST
-    const postApi = (path, data) => {
-        const url = `${baseUrl}${path}/`
-        axios.post(url, data)
-        .then(res => {
-            console.log(res.data)
-            setInfoApi([...infoApi, res.data])
-        })
-        .catch(err => console.log(err))
-    }
+  // DELETE
+  const deleteApi = (path, id, errorCallback) => {
+    const url = `${baseUrl}${path}/${id}/`;
+    axios.delete(url)
+      .then(() => {
+        setInfoApi((prevInfoApi) => prevInfoApi.filter((e) => e.id !== id));
+      })
+      .catch((err) => errorCallback ? errorCallback(err) : console.error(err));
+  };
 
-    //DELETE
-    const deleteApi = (path, id) => {
-        const url = `${baseUrl}${path}/${id}/`
-        axios.delete(url)
-            .then(res => {
-                setInfoApi(infoApi.filter(e => e.id !== id))
-            })
-            .catch(err => console.log(err))
-    }
+  // UPDATE
+  const updateApi = (path, id, data, errorCallback) => {
+    const url = `${baseUrl}${path}/${id}/`;
+    axios.patch(url, data)
+      .then((res) => {
+        setInfoApi((prevInfoApi) => prevInfoApi.map((e) => (e.id === id ? res.data : e)));
+      })
+      .catch((err) => errorCallback ? errorCallback(err) : console.error(err));
+  };
 
-    //UPDATE
-    const updateApi = (path, id, data) => {
-        const url = `${baseUrl}${path}/${id}/`
-        axios.patch(url, data)
-            .then(res => {
-                console.log(res.data)
-                setInfoApi(infoApi.map(e => e.id === id ? res.data : e))
-            })
-            .catch(err => console.log(err))    
-    }
+  return [infoApi, getApi, postApi, deleteApi, updateApi];
+};
 
-    return [ infoApi, getApi, postApi, deleteApi, updateApi  ]
-}
-
-export default useCrud
+export default useCrud;
